bad3ae954b1cfc4e2c5974199ade9f9f6e831570
Gustavo Oliveira <gustavo.oliveira@segurosunimed.com.br>
2026-08-26 17:02:33 -0300

    feat: adding Create and FindById methods for product

--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"log"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func Connect(ctx context.Context, uri string) (*mongo.Client, error) {
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
--- internal/repository/product.go
+++ internal/repository/product.go
@@ -2,8 +2,11 @@ package repository
 
 import (
 	"context"
-	"estoque/domain"
-	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/notoliveira/estoque/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type ProductRepository interface {
@@ -26,6 +29,21 @@ func NewMongoProductRepository(collection *mongo.Collection) ProductRepository {
 }
 
 func (r *mongoProductRepository) Create(ctx context.Context, product *domain.Product) error {
+
+	res, err := r.collection.InsertOne(ctx, product)
+	if err != nil {
+		return err
+	}
+
+	id := res.InsertedID
+
+	// Type assertion -> Dizer ao Go qual o tipo do objeto
+	// Nesse caso, estamos "transformando" o res.InsertedId (do tipo any) em primitive.ObjectId ao fazer id.(primitive.ObjectID)
+	// Esse .() serve justamente para "tipar" o objeto
+    mongoId := id.(primitive.ObjectID)
+
+	product.ID = mongoId
+
 	return nil
 }
 
@@ -34,7 +52,30 @@ func (r *mongoProductRepository) FindAll(ctx context.Context) ([]*domain.Product
 }
 
 func (r *mongoProductRepository) FindByID(ctx context.Context, id string) (*domain.Product, error) {
-	return nil, nil
+
+	// Transformando o id em primitive.ObjectId
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+        return nil, err
+    }
+	
+	// Criando o filtro (_id = <id>)
+	f := bson.M{"_id": objID}
+
+	// Criando product
+	var product domain.Product
+
+	err = r.collection.FindOne(ctx, f).Decode(&product)
+	if err != nil {
+        // Se o erro for "não encontrou nada", você pode retornar nil para o produto
+        if err == mongo.ErrNoDocuments {
+            return nil, nil
+        }
+        // Se for outro erro (ex: banco caiu), retorna o erro real
+        return nil, err
+    }
+
+	return &product, nil
 }
 
 func (r *mongoProductRepository) Update(ctx context.Context, id string, product *domain.Product) error {
